fdf0f369a592a71d3f4e8c4a0a133798e58a86c0
James Dietrich <dietrich.james@gmail.com>
2015-10-10 12:38:17 +0200

    Upgrade to Express 4

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-var express = require('express'),
-    jwt     = require('./routes/jwt'),
-    saml    = require('./routes/saml'),
-    logout  = require('./routes/logout'),
-    http    = require('http'),
-    path    = require('path');
+var express    = require('express'),
+    favicon    = require('serve-favicon'),
+    logger     = require('morgan'),
+    bodyParser = require('body-parser'),
+    jwt        = require('./routes/jwt'),
+    saml       = require('./routes/saml'),
+    logout     = require('./routes/logout'),
+    http       = require('http'),
+    path       = require('path');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.favicon('./public/images/tso.ico'));
-app.use(express.logger());
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(app.router);
+app.use(favicon('./public/images/tso.ico'));
+app.use(logger());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
