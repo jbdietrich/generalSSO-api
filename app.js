f2af308e86fb2a741b7430a3ee61e8aa38330030
James Dietrich <dietrich.james@gmail.com>
2015-10-11 15:46:22 +0200

    Remove logout code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express    = require('express'),
     bodyParser = require('body-parser'),
     jwt        = require('./routes/jwt'),
     saml       = require('./routes/saml'),
-    logout     = require('./routes/logout'),
     http       = require('http'),
     path       = require('path');
 
@@ -21,7 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/jwt', jwt.generate);
 app.post('/saml', saml.generate);
-app.get('/logout', logout.display);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/logout.js
+++
@@ -1,9 +0,0 @@
-exports.display = function(req, res){
-
-  var request = { params: req.query,
-                  path:   req.path,
-                  method: req.method,
-                  origin: (req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress };
-
-  res.render('logout', { request: JSON.stringify(request, undefined, 2) });
-};
