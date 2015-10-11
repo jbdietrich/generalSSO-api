ebeb279111f8d9b7697e4ab911fd840e831674f8
James Dietrich <dietrich.james@gmail.com>
2015-10-11 16:22:08 +0200

    Remove public folder, view logic

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 var express    = require('express'),
-    favicon    = require('serve-favicon'),
     logger     = require('morgan'),
     bodyParser = require('body-parser'),
     jwt        = require('./routes/jwt'),
     saml       = require('./routes/saml'),
-    http       = require('http'),
-    path       = require('path');
+    http       = require('http');
 
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(favicon('./public/images/tso.ico'));
 app.use(logger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/jwt', jwt.generate);
 app.post('/saml', saml.generate);
